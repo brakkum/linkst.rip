c70a14067fbceee5e233bcc83120724cfccf4819
Daniel Brakke <brakphoto@gmail.com>
2018-09-23 18:33:43 -0500

    initial error handle

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Link;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +13,14 @@ class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
      * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
 
         $link = new Link();
+        $link->setDomain("test");
 
         $form = $this->createFormBuilder($link)
             ->add("full_url", TextType::class, array(
@@ -38,61 +41,55 @@ class IndexController extends AbstractController
                     "placeholder" => "Custom Slug",
                 ),
             ))
-            ->getForm();
-
-        return $this->render('index/index.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="newLink")
-     * @param Request $request
-     * @return string
-     */
-    public function newLink(Request $request)
-    {
-        $link = new Link();
-        $form = $this->createFormBuilder($link)
+            ->add("save", SubmitType::class, array(
+                "label" => "Make Link",
+                "attr" => array(
+                    "class" => "",
+                )
+            ))
             ->getForm();
 
         $form->handleRequest($request);
 
         $task = $form->getData();
 
-        // check that custom slug doesn't exist
-        $custom_slug = $request->request->get("form")["slug"];
-        if ($custom_slug) {
-            /** @var \App\Repository\LinkRepository $link_repo */
-            $link_repo = $this->getDoctrine()->getRepository(Link::class);
-            /** @var \App\Entity\Link $link */
-            $link = $link_repo->findOneBy(array("slug" => $custom_slug));
-
-            if ($link) {
-                return $this->redirect("/err?1");
-            }
-        }
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // check that custom slug doesn't exist
+            $custom_slug = $request->request->get("form")["slug"];
+            if ($custom_slug) {
+                /** @var \App\Repository\LinkRepository $link_repo */
+                $link_repo = $this->getDoctrine()->getRepository(Link::class);
+                /** @var \App\Entity\Link $link */
+                $link = $link_repo->findOneBy(array("slug" => $custom_slug));
+
+                if ($link) {
+                    return $this->redirect("/err=0");
+                }
+            }
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($task);
             $em->flush();
 
             return $this->redirect("/");
         } else {
-            return $this->redirect("/");
+            return $this->render('index/index.html.twig', [
+                'form' => $form->createView(),
+            ]);
         }
     }
 
     /**
-     * @Route("/err{error_id}", name="errorDisplay", requirements={"error_id"="[0]"})
+     * @Route("/err={error_id}", name="errorDisplay", requirements={"error_id"="[0-1]"})
      * @param int $error_id
      * @return string
      */
     public function errorDisplay($error_id)
     {
         $errors = array(
-            0 => "zero",
+            0 => "Hmm.. Looks like that slug is already taken.",
+            1 => "one",
         );
 
         return $this->render('index/error.html.twig', [
